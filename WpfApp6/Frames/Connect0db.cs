dcde861ef1a5feefb43448d8623c39aaa6dd45ee
Александр Овчинников <shadow000fox@gmail.com>
2021-12-26 21:49:10 +0300

    dasd

---
+++ WpfApp6/Frames/Connect0db.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp6.Frames
+{
+    class Connect0db
+    {
+        public static ProducrtEnt con0Db = new ProducrtEnt();
+    }
+}
---
+++ WpfApp6/PAges/PageMain.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp6.Frames;
+
+namespace WpfApp6.PAges
+{
+    /// <summary>
+    /// Логика взаимодействия для PageMain.xaml
+    /// </summary>
+    public partial class PageMain : Page
+    {
+        public PageMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Product_Click(object sender, RoutedEventArgs e)
+        {
+            FrameObj.frameMain.Navigate(new PageProduct());
+        }
+    }
+}
